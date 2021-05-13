6ec05d8b37828e2e5f84a87f6cbe530e26a9f81f
Sakigami Yang <sakigami@163.com>
2021-05-13 21:47:35 +0800

    update

--- pylambda/combinators.py
+++ pylambda/combinators.py
@@ -16,14 +16,20 @@ K = TRUE
 S = lambda x: lambda y: lambda z: x(z)(y(z))
 
 # Fixed-point combinator
+#   Y = λf.(λx.f(x x)) (λx.f(x x))
 # Proof:
-#   Y g = λf.(λx.f(x x)) (λf.(λx.f(x x))) g
+#   Y g = λf.(λx.f(x x)) (λx.f(x x)) g
 #       = (λx.g(x x)) (λx.g(x x))  (by β-reduction of λf: applied Y to g)
-#       = (λy.g(y y)) (λx.g(x x))  (by α-conversion of left function: change x to y)
+#       = (λy.g(y y)) (λx.g(x x))  (by α-conversion of left function: rename x to y)
 #       = g ((λx.g(x x)) (λx.g(x x)))  (by β-reduction of λx: applied left function to right function)
 #       = g (Y g)
 #
 # We also have: Y = S(K(SII))(S(S(KS)K)(K(SII)))
+#
+# In fact, the implementation here is an operator called Z, where
+#   Z = λf.(λx.f(λy.x x y)) (λf.(λy.x x y))
+# because that Y needs "lazy evaluation".
+# In Python, if we run Y immediately, it will BOOM by infinite loop.
 Y = lambda f: (
     (lambda x: f(lambda y: x(x)(y)))
     (lambda x: f(lambda y: x(x)(y)))
